76a3a552680775d5343b6b0c5c07a61f51424d77
behemothbucket <mahagon.here@gmail.com>
2024-04-11 15:53:23 +0300

    update: lsp saga

--- colors/alabaster.lua
+++ colors/alabaster.lua
@@ -506,7 +506,7 @@ if vim.o.background == "dark" then
 		["@property.json"] = { link = "@AlabasterDefinition" },
 		-- LspSaga
 		["SagaBeacon"] = { bg = def_fg },
-		["SagaImpIcon"] = { bg = def_fg },
+		["SagaImpIcon"] = { fg = def_fg },
 	}
 ------------------------------------------------------------------------------
 ---  Light theme
