fb3b8bd839ba6affb06df1e9213ca14a4db3f288
behemothbucket <mahagon.here@gmail.com>
2024-04-22 20:14:49 +0300

    update: bg

--- colors/alabaster.lua
+++ colors/alabaster.lua
@@ -107,9 +107,9 @@ if vim.o.background == "dark" then
 		MoreMsg = { fg = ansi.green, bold = 1 },
 		NonText = { fg = "#696969" },
 		-- Normal = { bg = "none", fg = fg },
-		-- NormalFloat = { bg = "none" },
+		NormalFloat = { bg = "none" },
 		Normal = { bg = bg, fg = fg },
-		NormalFloat = { bg = float_bg },
+		-- NormalFloat = { bg = float_bg },
 		-- NormalNC     { }, -- normal text in non-current windows
 		FloatBorder = { bg = "none", fg = "none" },
 		Pmenu = { bg = "none" },
