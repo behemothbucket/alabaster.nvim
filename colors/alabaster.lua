832f1124ea5d42abc2fa52813c459accf114057e
Maksim Rozhkov <mrozhkov@rubackup.ru>
2025-03-08 19:15:24 +0700

    update: Normal, NormalFloat

--- colors/alabaster.lua
+++ colors/alabaster.lua
@@ -100,10 +100,10 @@ if vim.o.background == "dark" then
 		-- MsgSeparator { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
 		MoreMsg = { fg = ansi.green, bold = 1 },
 		NonText = { fg = "#696969" },
-		Normal = { bg = "none", fg = fg },
-		NormalFloat = { bg = "none" },
-		-- Normal = { bg = bg, fg = fg },
-		-- NormalFloat = { bg = float_bg },
+		-- Normal = { bg = "none", fg = fg },
+		-- NormalFloat = { bg = "none" },
+		Normal = { bg = bg, fg = fg },
+		NormalFloat = { bg = float_bg },
 		-- NormalNC     { }, -- normal text in non-current windows
 		FloatBorder = floatborder,
 		-- FloatBorder = { bg = "none", fg = "none" },
